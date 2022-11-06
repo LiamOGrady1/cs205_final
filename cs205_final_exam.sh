8f74f17b5045a9d0495849aaa8c7204050bc79b1
LiamOGrady1 <104698935+LiamOGrady1@users.noreply.github.com>
2022-11-05 21:21:30 -0700

    Update cs205_final_exam.sh
    As per the instructions commented in. I attempted to output file name, how many there are, their average hp, and their average attack. This was rather difficult in some ways. For the name I simply printed it for the sake of simplicity to output pokemon.dat, as that is the only file we will use for this program. For the number of pokemon we only had to count the lines. For the average hp and attack, I found it was easiest to loop through and add up the values for hp and attack, then divide them by the sum (which we've added up with each loop).

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,29 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+BEGIN{FS="\t"}
+{
+if (NR!=1){
+#add up hp
+hp+=$1
+
+sum+=1
+}
+}
+END{
+#print output+words
+print 'avg. HP:' hp/sum}
+
+BEGIN{FS="\t"}
+{
+if (NR!=1){
+#add up hp
+atk+=$1
+
+sum+=1
+}
+}
+END{
+#print output+words
+print 'avg. HP:' atk/sum}
